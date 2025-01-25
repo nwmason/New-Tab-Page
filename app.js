2fcaac814a6f3fb6d6d45119878067deee2ad5e9
nwmason <octavee6@gmail.com>
2025-01-25 09:21:40 -0500

    nevermind adding google functionality is literally the same, functionality added

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const searchBox = document.getElementById('search-box')
 const osrsWikiSearch = document.getElementById('osrs-wiki-search');
 
+searchBox.addEventListener('keypress', function(e) {
+    const query = searchBox.value.replaceAll(" ", "+");
+    const website = "https://www.google.com/search?q=";
+    const searchUrl = website + query;
+
+    if (e.key === "Enter") {
+        window.open(searchUrl, '_blank').focus();
+    }
+})
+
 osrsWikiSearch.addEventListener('keypress', function(e) {
-    const query = osrsWikiSearch.value;
+    const query = osrsWikiSearch.value.replaceAll(" ", "+");
     const website = "https://oldschool.runescape.wiki/?search=";
     const searchUrl = website + query;
 
-    // window.location.href = searchUrl;
     if (e.key === "Enter") {
         window.open(searchUrl, '_blank').focus();
     } 
