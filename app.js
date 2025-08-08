532faf50508e5cbdaee97950c73f350be8765e9d
nwmason <octavee6@gmail.com>
2025-08-08 19:21:17 -0400

    removed anti google AI URL because it breaks embeds and I need that for the NFL schedule

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const osrsWikiSearch = document.getElementById('osrs-wiki-search');
 searchBox.addEventListener('keypress', function(e) {
     const query = searchBox.value.replaceAll(" ", "+");
     const website = "https://www.google.com/search?q=";
+    const searchURL = website + query;
 
     if (e.key === "Enter") {
-        window.open(searchUrl, '_blank').focus();
+        window.open(searchURL, '_blank').focus();
         searchBox.value = "";
     }
 })
@@ -14,10 +15,10 @@ searchBox.addEventListener('keypress', function(e) {
 osrsWikiSearch.addEventListener('keypress', function(e) {
     const query = osrsWikiSearch.value.replaceAll(" ", "+");
     const website = "https://oldschool.runescape.wiki/?search=";
-    const searchUrl = website + query;
+    const searchURL = website + query;
 
     if (e.key === "Enter") {
-        window.open(searchUrl, '_blank').focus();
+        window.open(searchURL, '_blank').focus();
         osrsWikiSearch.value = "";
     } 
 });
